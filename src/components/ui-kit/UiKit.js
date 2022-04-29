b79749854fa0e13240ee347e69a28ea4c15f4710
kkewno <kkewno@mail.ru>
2022-04-29 11:27:30 +0300

    ui-kit(without pop-up)

--- src/components/ui-kit/UiKit.js
+++ src/components/ui-kit/UiKit.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../ui/Button/Button';
 import '../../styles/kitStyle.scss';
 import PopUp from '../ui/PopUp/PopUp'
 import Input from '../ui/Input/Input'
 import Product from '../ui/Product/Product';
+import Basket from '../Header/Basket/Basket';
 
 const UiKit = (props) => {
+
     return (
         <div className='container'>
             {/* <Picture src='../../img/product.png'/> */}
             <div className='container__wrap-button'>
-                <Button text='Добавить в корзину' class='container__wrap-button__button'/>
-                <Button text='Добавить в корзину' class='container__wrap-button__button container__wrap-button__button__hover'/>
-                <Button text='. . .' class='container__wrap-button__button container__wrap-button__button__load'/>
-                <Button text='Добавить в корзину' class='container__wrap-button__button container__wrap-button__button__disable'/>
+                <Button href='/'>
+                    Добавить в корзину
+                </Button>
+                <Button href='/'>
+                    . . .
+                </Button>
+                <Button disable={true} href='/'>
+                    Добавить в корзину
+                </Button>
             </div>
 
             <div className='container__pop-up'>
-                <PopUp/>
+                <PopUp>
+                    <div className='pop-up'>
+                        <div className='pop-up__header'>
+                            <p className='pop-up__header__text'>{props.text}</p>
+                            <img className='pop-up__header__picture' src={require('../../img/close.png')}/>
+                        </div>
+                    </div>
+                </PopUp>
+                <button className='container__pop-up__button'>Открыть</button>
             </div>
 
             <div className='container__inputs'> 
-                <Input type='text' text='Имя'/>
-                <Input type='text' text='Имя' error={true} textError='Обязательное поле'/>
+                <Input type='text' text='Имя'>Имя</Input>
+                <Input type='text' text='Имя' textError='Обязательное поле'>Имя</Input>
             </div>
 
             <div className='container__picture'>
-                <Product src={'product.png'}/>
-                {/* <img src={require('../../img/product.png')} alt='' className='container__pop-up__header__close'/> */}
+                <Product src={'product.png'} name='Стул рабочий' price='6 000 ₽' textButton='Добавить в корзину'/>
             </div>
 
         </div>
